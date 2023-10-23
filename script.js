5a18852132136865a3dd70384c7884515258a9d2
Caio Guilherme <caioguilherme@MacBook-Air-de-Caio.local>
2023-10-23 10:35:44 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var typingEffect = new Typed(".multiText", {
+    strings: ["Caio Guilherme", "Estudante", "Estagiário",],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1500
+});
+
+
+const navItens = document.querySelector(".navItens");
+
+
+const navItensSobre = document.querySelector("#navItens");
+
+if (navItens && navItensSobre) {
+    navItensSobre.innerHTML = navItens.innerHTML;
+}
